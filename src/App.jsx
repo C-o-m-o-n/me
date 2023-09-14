00ca61e171ac00cf70822f222dc289cc88c26c89
Collins O. Odhiambo <comon928@gmail.com>
2023-09-14 16:25:12 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Collins Omondi.",
+  title: "backend developer",
+  email: "comon928@gmail.com",
+  gitHub: "C-o-m-o-n",
+  // instagram: "microsoft",
+  linkedIn: "c-o-m-o-n",
+  medium: "comon928",
+  twitter: "C_o_m_o_n",
+  youTube: "comon928",
 };
 
 const primaryColor = "#4E567E";
