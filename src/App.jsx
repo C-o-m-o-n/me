113c57bf7b500d760f10048e5166a210b1b9e1e7
C-o-m-o-n <comon928@gmail.com>
2023-12-20 04:09:57 +0300

    add annimation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 // import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
+// import { useRef } from 'react'
 import './App.css'
 import Cube from './components/Cube'
 // import { BoxGeometry } from 'three'
@@ -10,13 +11,15 @@ function App() {
 
   return (
     <Canvas>
-      <directionalLight position={[0, 0, 5]} />
-<group position={[0, -1, 0]}>
+      <directionalLight position={[0, 0, 5]} intensity={0.5}/>
+      <ambientLight intensity={0.1} />
+{/* <group position={[0, -1, 0]}>
       <Cube  position={[1, 0, 0]} size={[1, 1, 1]}  color="green" />
       <Cube  position={[-1, 2, 0]} size={[1, 1, 1]}  color="blue" />
       <Cube  position={[1, 2, 0]} size={[1, 1, 1]}  color="red" />
       <Cube  position={[-1, 0, 0]} size={[1, 1, 1]}  color="yellow" />
-</group>
+</group> */}
+      <Cube  position={[0, 0, 0]} size={[1, 1, 1]}  color="yellow" />
     </Canvas>
   )
 }
--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 // import React from 'react'
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
 
 function Cube( {position, size , color} ) {
+    const ref = useRef()
+
+  useFrame((state, delta) => {
+    ref.current.rotation.X += delta
+    // console.log(delta)
+  })
+
   return (
-    <mesh position={position}>
+    <mesh position={position} ref={ref}>
         <boxGeometry args={size}/>
         <meshStandardMaterial color={color}/>
       </mesh>
